7139f92bc1d6a64998759a8cb7cfc804fd8905d6
Kandarp Khandwala <kandarpck@gmail.com>
2018-03-14 20:45:46 -0400

    Add the basic implementations of the miners in the network

---
+++ src/main/java/cs441641/miners/CompliantMiner.java
@@ -0,0 +1,50 @@
+package cs441641.miners;
+
+import cs441641.blockchain.Block;
+import cs441641.blockchain.NetworkStatistics;
+
+public class CompliantMiner extends BaseMiner implements Miner {
+    private Block currentHead;
+
+    public CompliantMiner(String id, int hashRate, int connectivity) {
+        super(id, hashRate, connectivity);
+
+    }
+
+    @Override
+    public Block currentlyMiningAt() {
+        return currentHead;
+    }
+
+    @Override
+    public Block currentHead() {
+        return currentHead;
+    }
+
+    @Override
+    public void blockMined(Block block, boolean isMinerMe) {
+        if (isMinerMe) {
+            if (block.getHeight() > currentHead.getHeight()) {
+                this.currentHead = block;
+            }
+        } else {
+            if (currentHead == null) {
+                currentHead = block;
+            } else if (block != null && block.getHeight() > currentHead.getHeight()) {
+                this.currentHead = block;
+
+            }
+        }
+    }
+
+
+    @Override
+    public void initialize(Block genesis, NetworkStatistics networkStatistics) {
+        this.currentHead = genesis;
+    }
+
+    @Override
+    public void networkUpdate(NetworkStatistics statistics) {
+
+    }
+}
